2d51db79b06fe29a3ba5e451ecaf02cea0304373
ghkdtnql <ghkdtnql@gmail.com>
2025-03-10 13:07:28 +0900

    👾chore: 백엔드 프로젝트 초기 세팅

---
+++ backend/farmbti/src/test/java/com/backend/farmbti/FarmbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.farmbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmbtiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
