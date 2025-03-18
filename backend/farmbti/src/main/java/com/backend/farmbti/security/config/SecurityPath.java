3503c2ae0ba9877ca03fe85116318c65c981e8f5
ì Nageum Jeong <nagum0545@naver.com>
2025-03-18 15:53:38 +0900

    fix: SecurityPath 수정

--- backend/farmbti/src/main/java/com/backend/farmbti/security/config/SecurityPath.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/security/config/SecurityPath.java
@@ -11,7 +11,7 @@ public enum SecurityPath {
     //Swagger 관련 경로
     //Swagger 관련 경로
     SWAGGER_UI("/swagger-ui/**"),
-    SWAGGER_API_DOCS("/v3/api-docs"),
+    SWAGGER_API_DOCS("/v3/api-docs/"),
     SWAGGER_API_DOCS_ALL("/v3/api-docs/**"),
     SWAGGER_RESOURCES("/swagger-resources/**"),
     SWAGGER_WEBJARS("/webjars/**"),  // 추가 필요
