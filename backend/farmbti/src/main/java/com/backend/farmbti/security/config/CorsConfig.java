be2b964c1978587d14f3b57ec17a52ff58893cac
ì Nageum Jeong <nagum0545@naver.com>
2025-03-18 15:51:19 +0900

    fix: SecurtyPath 수수수정

--- backend/farmbti/src/main/java/com/backend/farmbti/security/config/CorsConfig.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/security/config/CorsConfig.java
@@ -19,7 +19,8 @@ public class CorsConfig {
             "http://localhost:3000",    // 로컬 개발 환경
             "http://localhost:8080",
             "http://j12d209.p.ssafy.io",
-            "http://j12d209.p.ssafy.io:8080/swagger-ui/index.html"
+            "http://j12d209.p.ssafy.io:8080/swagger-ui/index.html",
+            "http://j12d209.p.ssafy.io:8080/v3/api-docs"
         ));
 
         // 허용할 HTTP 메서드 설정
