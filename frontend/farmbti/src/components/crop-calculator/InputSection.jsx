f2b6dd09431166134359aaacea98c69980049a6c
Sunhwa Sim <sh227sim@gmail.com>
2025-03-20 18:01:21 +0900

    ✨ feat: 스텝 안내 섹션 컴포넌트 생성

---
+++ frontend/farmbti/src/components/crop-calculator/InputSection.jsx
@@ -0,0 +1,9 @@
+const InputSection = () => {
+    return (<div id="crop-calculator-input">
+        <div>
+            <input type="number" name="area-input" id="area-input" />
+        </div>
+    </div>);
+}
+
+export default InputSection;
