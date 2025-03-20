bba54cb9abd474c34790df2fab15026b30144a8c
shsim <sh227sim@gmail.com>
2025-03-20 23:37:32 +0900

    🐛 fix: 이름 수정

--- frontend/farmbti/src/components/crop-calculator/InputSection.jsx
+++
@@ -1,9 +0,0 @@
-const InputSection = () => {
-    return (<div id="crop-calculator-input">
-        <div>
-            <input type="number" name="area-input" id="area-input" />
-        </div>
-    </div>);
-}
-
-export default InputSection;
