68e558eb729c0ab143e48d80eb64acbddbca2717
Sunhwa Sim <sh227sim@gmail.com>
2025-03-20 17:22:54 +0900

    ✨ feat: 작물 계산기 헤더 추가

--- frontend/farmbti/src/pages/crop-calculator/CropCalculatorPage.jsx
+++ frontend/farmbti/src/pages/crop-calculator/CropCalculatorPage.jsx
@@ -1,9 +1,14 @@
+import PageHeader from "../../components/common/PageHeader";
+
 const CropCalculatorPage = () => {
   return (
     <div>
-      <h1>작물 수확 계산기</h1>
+      <PageHeader
+        title="작물 수익 계산기"
+        description="원하는 작물 재배에 따른 예상 수익을 계산해보세요."
+      />
     </div>
   );
-}
+};
 
 export default CropCalculatorPage;
