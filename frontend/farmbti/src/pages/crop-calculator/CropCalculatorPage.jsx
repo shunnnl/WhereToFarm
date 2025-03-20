031fc472e11bcde203dd79c49ff24b576d8a3ba0
Sunhwa Sim <sh227sim@gmail.com>
2025-03-20 18:01:46 +0900

    ✨ feat: 작물 수익 계산기 스텝, 입력 컴포넌트 추가

--- frontend/farmbti/src/pages/crop-calculator/CropCalculatorPage.jsx
+++ frontend/farmbti/src/pages/crop-calculator/CropCalculatorPage.jsx
@@ -1,4 +1,6 @@
 import PageHeader from "../../components/common/PageHeader";
+import InputSection from "../../components/crop-calculator/InputSection";
+import StepSection from "../../components/crop-calculator/StepSection";
 
 const CropCalculatorPage = () => {
   return (
@@ -7,6 +9,10 @@ const CropCalculatorPage = () => {
         title="작물 수익 계산기"
         description="원하는 작물 재배에 따른 예상 수익을 계산해보세요."
       />
+      <div className="crop-calculator-container w-full bg-gray-100">
+        <StepSection/>
+        <InputSection/>
+      </div>
     </div>
   );
 };
