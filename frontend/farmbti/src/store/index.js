7d44177483f129b0180d8f328eab66700ab40a6b
최미연 <mu05041@naver.com>
2025-03-21 15:37:32 +0900

    feat: signup axios 추가

--- frontend/farmbti/src/store/index.js
+++ frontend/farmbti/src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
-import signupReducer from './signupSlice';
+import authReducer from './slices/authSlice';
+import signupReducer from './slices/signupSlice';
 
 export const store = configureStore({
   reducer: { 
