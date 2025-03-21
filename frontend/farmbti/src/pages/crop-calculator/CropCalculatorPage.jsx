7aff29a418586fb9685b78daeb9880480c111de5
Sunhwa Sim <sh227sim@gmail.com>
2025-03-21 09:16:59 +0900

    ✨ feat: 작물 계산기 페이지에서 평수 입력 칸 props 전달

--- frontend/farmbti/src/pages/crop-calculator/CropCalculatorPage.jsx
+++ frontend/farmbti/src/pages/crop-calculator/CropCalculatorPage.jsx
@@ -6,6 +6,20 @@ import { useState } from "react";
 
 const CropCalculatorPage = () => {
   const [step, setStep] = useState(1);
+  const [area, setArea] = useState(0);
+  const [convertedArea, setConvertedARea] = useState(0);
+  const [crop, setCrop] = useState(null);
+  const [result, setResult] = useState(null);
+  const [isLoding, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleAreaSubmit = () => {
+    // 평을 제곱미터 변환
+    setConvertedARea(area * 3.3058);
+    // 변환 후 단계 넘어감
+    setStep(2);
+  };
+
   return (
     <div>
       <PageHeader
@@ -13,8 +27,14 @@ const CropCalculatorPage = () => {
         description="원하는 작물 재배에 따른 예상 수익을 계산해보세요."
       />
       <div className="crop-calculator-container w-auto mx-10 bg-background">
-        <ProgressIndicator currentstep={step}/>
-        <AreaInputSection/>
+        <ProgressIndicator currentstep={step} />
+        <AreaInputSection
+          area={area}
+          setArea={setArea}
+          convertedArea={convertedArea}
+          onSubmit={handleAreaSubmit}
+          isActive={step === 1}
+        />
       </div>
     </div>
   );
