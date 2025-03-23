5c0982118fce0376ac993aa43c6802c4539a0075
shsim <sh227sim@gmail.com>
2025-03-23 20:10:36 +0900

    ✨ feat: 월별 시세 예측 그래프 파트

---
+++ frontend/farmbti/src/components/crop-calculator/BenefitForecastGraph.jsx
@@ -0,0 +1,11 @@
+const BenefitForecastGraph = ({ myForecast }) => {
+  return (
+    <div className="bg-accentGreen-light rounded-lg shadow-md m-8 p-6 flex flex-col justify-center items-center">
+      <div className="text-center mb-5">
+        <p className="text-2xl font-bold mb-2">월별 작물 매출액 예상 그래프</p>
+      </div>
+    </div>
+  );
+};
+
+export default BenefitForecastGraph;
