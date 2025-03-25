077602008a24a4565cd3c9b7aa0136c49251dcf6
ì Nageum Jeong <nagum0545@naver.com>
2025-03-25 15:00:06 +0900

    fix: Swagger 관련 파일 수정

--- backend/farmbti/src/main/java/com/backend/farmbti/common/config/SwaggerConfig.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/common/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.backend.farmbti.common.config;
 
+import java.util.List;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -31,6 +33,8 @@ public class SwaggerConfig {
             .info(new Info()
                 .title("FarmBTI API")
                 .description("FarmBTI 서비스의 API 문서")
-                .version("1.0.0"));
+                .version("1.0.0"))
+            .servers(List.of(new Server().url("/api")));
+
     }
 }
