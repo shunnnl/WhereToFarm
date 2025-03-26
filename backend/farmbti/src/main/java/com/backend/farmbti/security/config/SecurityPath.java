ca3f236a7c128d9b56e97e1de776171ab1c54007
ì Nageum Jeong <nagum0545@naver.com>
2025-03-26 19:08:37 +0900

    [INFRA] SecurityPath - swagger 수정

--- backend/farmbti/src/main/java/com/backend/farmbti/security/config/SecurityPath.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/security/config/SecurityPath.java
@@ -15,12 +15,6 @@ public enum SecurityPath {
     SWAGGER_RESOURCES("/swagger-resources/**"),  // 추가된 부분
     SWAGGER_HTML("/swagger-ui.html"),
 
-    SWAGGER_UI_API_DOCS("/v3/api-docs"),
-    SWAGGER_UI_API_DOCS_ALL("/v3/api-docs/**"),
-    SWAGGER_UI_RESOURCES("/swagger-resources/**"),
-    SWAGGER_UI("/swagger-ui/**"),
-    SWAGGER_UI_HTML("/api/swagger-ui.html"),
-
     // Auth 관련 경로
     SIGNUP("/auth/signUp"),
     LOGIN("/auth/login"),
