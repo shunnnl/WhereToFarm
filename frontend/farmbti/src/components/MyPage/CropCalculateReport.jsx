6655a260acced1de5afd76ed38b7bec8c7b8c057
Sunhwa Sim <sh227sim@gmail.com>
2025-03-26 17:10:17 +0900

    ⚰️ refector: 안쓰는 코드 삭제

--- frontend/farmbti/src/components/MyPage/CropCalculateReport.jsx
+++ frontend/farmbti/src/components/MyPage/CropCalculateReport.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import CalculateResultCard from "./CalculateResultCard";
 import PaginationComponent from "../common/Pagination";
 
