fd811f364c56b90b19c81bd4993ee2af4209e17e
ì Nageum Jeong <nagum0545@naver.com>
2025-03-26 19:47:55 +0900

    [INFRA] Swagger 설정 수정 🙏🏻😭🙏🏻

--- backend/farmbti/src/main/java/com/backend/farmbti/security/config/SecurityPath.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/security/config/SecurityPath.java
@@ -10,10 +10,15 @@ public enum SecurityPath {
 
     //Swagger 관련 경로
     SWAGGER("/swagger-ui/**"),
-    SWAGGER_API_DOCS("/v3/api-docs"),
-    SWAGGER_API_DOCS_ALL("/v3/api-docs/**"),  // 추가된 부분
-    SWAGGER_RESOURCES("/swagger-resources/**"),  // 추가된 부분
     SWAGGER_HTML("/swagger-ui.html"),
+    SWAGGER_API_DOCS("/v3/api-docs"),
+    SWAGGER_API_DOCS_ALL("/v3/api-docs/**"),
+    SWAGGER_RESOURCES("/swagger-resources/**"),
+    SWAGGER_UI("/api/swagger-ui/**"),
+    SWAGGER_UI_API_DOCS("/api/v3/api-docs"),
+    SWAGGER_UI_API_DOCS_ALL("/api/v3/api-docs/**"),
+    SWAGGER_UI_RESOURCES("/api/swagger-resources/**"),
+    SWAGGER_UI_HTML("/api/swagger-ui.html"),
 
     // Auth 관련 경로
     SIGNUP("/auth/signUp"),
