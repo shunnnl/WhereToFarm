6aecb2e0be7394efd706e26837fb98105d355a75
siyun <siyun2072@naver.com>
2025-03-26 16:08:52 +0900

    Feat : 멘토 정보 수정

--- backend/farmbti/src/main/java/com/backend/farmbti/mentors/exception/MentorsErrorCode.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/mentors/exception/MentorsErrorCode.java
@@ -11,7 +11,8 @@ public enum MentorsErrorCode implements ErrorCode {
 
     ALREADY_REGISTERED_AS_MENTOR(400, "이미 멘토로 등록된 사용자입니다."),
     INVALID_MENTOR_INFO(400, "자기소개는 필수 항목입니다."),
-    INVALID_FARMING_YEARS(400, "영농 시작 년도는 필수 항목입니다.");
+    INVALID_FARMING_YEARS(400, "영농 시작 년도는 필수 항목입니다."),
+    MENTOR_NOT_FOUND(404, "해당 멘토를 찾을 수 없습니다.");
 
 
     private final int status;
