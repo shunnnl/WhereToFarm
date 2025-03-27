20114a6e850641b56f72640d523c5733db357bbf
최미연 <mu05041@naver.com>
2025-03-27 16:23:24 +0900

    feat: refresh토큰 확인함수 추가

--- frontend/farmbti/src/App.jsx
+++ frontend/farmbti/src/App.jsx
@@ -2,6 +2,9 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { useEffect } from 'react';
+import { checkTokenExpiration } from './store/slices/authSlice';
+import { useDispatch } from 'react-redux';
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/toast-custom.css";
 
@@ -35,7 +38,22 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
+
+  
   console.log("Store:", store);
+
+  // 리프레시 토큰 체크
+  useEffect(() => {
+    // 앱 로드 시 바로 체크
+    store.dispatch(checkTokenExpiration());
+    
+    // 주기적으로 토큰 만료 체크 (5분마다)
+    const intervalId = setInterval(() => {
+      store.dispatch(checkTokenExpiration());
+    }, 5 * 60 * 1000);
+    
+    return () => clearInterval(intervalId);
+  }, []);  // useDispatch를 의존성 배열에서 제거
 
   return (
     <Provider store={store}>
