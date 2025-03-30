6ffbf6789b5152cee01c479a46567feae7d03691
siyun <siyun2072@naver.com>
2025-03-31 02:10:24 +0900

    Feat : zepplin noteid yml으로 이동

--- backend/farmbti/src/main/java/com/backend/farmbti/report/controller/ReportController.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/report/controller/ReportController.java
@@ -7,6 +7,7 @@ import com.backend.farmbti.report.service.ZeppelinService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import com.backend.farmbti.common.dto.CommonResponseDto;
 
@@ -20,17 +21,19 @@ public class ReportController {
     private final ZeppelinService zeppelinService;
     private final ReportService reportService;
 
+    @Value("${zeppelin.notebook.id}")
+    private String defaultNoteId;
+
     @Operation(
             summary = "Zeppelin 노트북 실행 및 리포트 생성",
             description = "Zeppelin 노트북을 실행하고 결과로 나온 지역 정보를 바탕으로 리포트를 생성합니다."
     )
-    @PostMapping("/create/{noteId}")
-    public CommonResponseDto<ReportResponseDto> runNotebookWithParamsForLast(
-            @PathVariable String noteId,
+    @PostMapping("/create")
+    public CommonResponseDto<ReportResponseDto> createReport(
             @RequestBody ZeppelinRequestDto requestDto
     ) throws InterruptedException {
-        // 1. Zeppelin 서비스로 노트북 실행 및 지역 목록 추출
-        String raw = zeppelinService.runNotebookWithParamsForAll(noteId, requestDto.getParams());
+        // 1. Zeppelin 서비스로 노트북 실행 및 지역 목록 추출 (고정된 noteId 사용)
+        String raw = zeppelinService.runNotebookWithParamsForAll(defaultNoteId, requestDto.getParams());
         List<String> regionList = zeppelinService.extractTopRegions(raw); // 지역 리스트만 뽑아옴
 
         // 2. 추출된 지역 목록을 ReportService로 전달하여 리포트 생성
