870c031eb820b4b24b9a902f48787157437fc5b8
권가영 <kky3653@naver.com>
2025-04-03 01:19:51 +0900

    bugfix: report api 버그 수정

--- frontend/farmbti/src/components/recommendation/survey/ProgressBar.jsx
+++ frontend/farmbti/src/components/recommendation/survey/ProgressBar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-// assets 폴더에서 이미지를 import 합니다
 import progressIcon from '../../../asset/recommendation/survey/progress-icon.png';
 
 const ProgressBar = ({ progress }) => {
   return (
     <div className="max-w-4xl mx-auto px-4 mb-12">
-      <div className="relative w-full h-2 bg-[#E8F3E7] rounded-full overflow-visible">
+      <div className="relative w-full h-2 bg-[#E8F3E7] rounded-full">
         <div 
-          className="relative h-full bg-[#1B4B36] rounded-full transition-all duration-300 ease-in-out"
+          className="absolute left-0 h-full bg-[#1B4B36] rounded-full transition-[width] duration-300 ease-in-out"
           style={{ width: `${progress}%` }}
         />
+        
         <div
-          className="absolute top-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out"
-          style={{ left: `${progress}%` }}
+          className="absolute h-12 w-12 transition-[left] duration-300 ease-in-out"
+          style={{ 
+            left: `${Math.min(progress, 100)}%`,
+            top: '50%',
+            marginLeft: '-24px',
+            marginTop: '-24px'
+          }}
         >
           <img 
             src={progressIcon}
             alt="진행 상태"
-            className="w-12 h-12 object-contain -translate-x-1/2"
+            className="w-full h-full object-contain"
           />
         </div>
       </div>
@@ -25,4 +30,4 @@ const ProgressBar = ({ progress }) => {
   );
 };
 
-export default ProgressBar;
+export default ProgressBar; 
