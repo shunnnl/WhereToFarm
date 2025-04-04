c06353e773cb3fdacbcd3617dad51cde503f5a4b
ghkdtnql <ghkdtnql@gmail.com>
2025-04-04 09:45:10 +0900

    🐛fix: message content type 길이 수정

--- backend/farmbti/src/main/java/com/backend/farmbti/chat/entity/ChatMessage.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/chat/entity/ChatMessage.java
@@ -18,7 +18,7 @@ public class ChatMessage {
     @Column(name = "message_id")
     private Long messageId;
 
-    @Column(nullable = false, length = 1000)
+    @Column(nullable = false, length = 2000)
     private String content;
 
     @Column(name = "sender_id")
