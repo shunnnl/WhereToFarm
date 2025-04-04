587f66f90a79b29d52589ed983a72b7d9b2b054f
ghkdtnql <ghkdtnql@gmail.com>
2025-04-04 10:22:50 +0900

    🐛fix: server port

--- backend/farmbti/src/main/java/com/backend/farmbti/common/config/SwaggerConfig.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/common/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -30,7 +33,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("FarmBTI API")
                         .description("FarmBTI 서비스의 API 문서")
-                        .version("1.0.0"));
-        // .servers(List.of(new Server().url("https://j12d209.p.ssafy.io/api"))); // api 붙어야함!
+                        .version("1.0.0"))
+                .servers(List.of(new Server().url("https://j12d209.p.ssafy.io/api"))); // api 붙어야함!
     }
 }
