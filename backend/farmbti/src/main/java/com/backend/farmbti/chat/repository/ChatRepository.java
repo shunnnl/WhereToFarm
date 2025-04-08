47c8e82bbf6909f1d062110177f8224a8de56932
ghkdtnql <ghkdtnql@gmail.com>
2025-04-08 15:08:11 +0900

    🐛fix: annotation 추가

--- backend/farmbti/src/main/java/com/backend/farmbti/chat/repository/ChatRepository.java
+++ backend/farmbti/src/main/java/com/backend/farmbti/chat/repository/ChatRepository.java
@@ -2,10 +2,11 @@ package com.backend.farmbti.chat.repository;
 
 import com.backend.farmbti.auth.domain.Users;
 import com.backend.farmbti.chat.entity.Chat;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.List;
 import java.util.Optional;
